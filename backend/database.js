f2fef9a4edbd35743d191848009bd88a6f8b53b5
Farmaan <mfmohamed@students.nsbm.ac.lk>
2023-01-25 11:01:30 +0530

    Update database.js
    Changed religion to hometown

--- backend/database.js
+++ backend/database.js
@@ -173,7 +173,7 @@ const readStudentInfo = async (id) => {
 }
 
 const addStudent = async (id, name, age, religion) => {
-    const sql = `INSERT INTO student(id,name,age, religion) values (?, ?, ?, ?)`
+    const sql = `INSERT INTO student(id,name,age, hometown) values (?, ?, ?, ?)`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
@@ -187,7 +187,7 @@ const addStudent = async (id, name, age, religion) => {
 }
 
 const updateStudent = async (name, age, religion, id) => {
-    const sql = `UPDATE student SET name=?, age=?, religion=? WHERE id=?`
+    const sql = `UPDATE student SET name=?, age=?, hometown=? WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
